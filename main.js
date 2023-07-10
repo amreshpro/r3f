000b6fc9fcb5909db81c59212121a65b267e3ead
Amresh Maurya <amresh.terminal@gmail.com>
2023-07-10 13:04:07 +0530

    animation in box geometry

--- main.js
+++ main.js
@@ -1,38 +1,63 @@
-import *as THREE from 'three';
+import * as THREE from 'three'
 
+
+// get canvas id
 const canvas = document.getElementById("webgl")
 
-const scene = new THREE.Scene()
+// scene banao
+const scene = new THREE.Scene();
 
-// red cube
+
+// geometry
 const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:"#ff0000"})
-const mesh = new THREE.Mesh(geometry,material)
+
+// mesh k liye basic material
+const material = new THREE.MeshBasicMaterial({color:"orange"})
+
+// creating mesh
+const mesh  = new THREE.Mesh(geometry,material)
+
+// scene me mesh add kr diya
 scene.add(mesh)
 
-
 const size = {
-  width:800,
-  height:600,
+  width:window.innerWidth,
+  height:window.innerHeight,
 }
 
-// camera
-const camera  = new THREE.PerspectiveCamera(75,size.width/size.height)
-camera.position.z=3
-// camera.position.x=2
+
+// camera banao
+const camera = new THREE.PerspectiveCamera(75,size.width/size.height); //preferred
+
+
+// camera box k andar hi h to kuch show nhi hoga
+
+// changing camera position 
+camera.position.z = 3
+
+// camera add in scene
 scene.add(camera)
 
 
 
-console.log(canvas)
-// render
-const renderer = new THREE.WebGLRenderer({
-  canvas
-
-})
-
+const renderer = new THREE.WebGLRenderer({canvas})
 
 renderer.setSize(size.width,size.height)
+// renderer.render(scene,camera)
 
 
+
+// Animations
+function tick(a){
+ 
+  // update objects
+  mesh.rotation.y +=0.01
+mesh.rotation.x +=0.03
+
+  // render
 renderer.render(scene,camera)
+
+  window.requestAnimationFrame(tick)
+}
+
+tick("Amresh")
